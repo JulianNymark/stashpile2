6e2796336e9e2000a81df1e1302c65f26d289312
Julian <julianymark@gmail.com>
2018-07-23 23:01:19 +0200

    + tslint + messing about with some material renderers & light

--- app.ts
+++ app.ts
@@ -1,10 +1,20 @@
-import './main.scss';
 import * as three from 'three';
 
-var scene: three.Scene;
-var camera: three.Camera;
-var renderer: three.WebGLRenderer;
-var cube: three.Mesh;
+import './main.scss';
+
+let scene: three.Scene;
+let camera: three.Camera;
+let renderer: three.WebGLRenderer;
+const meshesToAnimate: three.Mesh[] = [];
+
+function addCuboid(scene: three.Scene, position: three.Vector3) {
+    const geometry = new three.BoxGeometry(0.5, 0.5, 2);
+    const material = new three.MeshPhongMaterial({ color: 0x00ff00 });
+    const cube = new three.Mesh(geometry, material);
+    scene.add(cube);
+    meshesToAnimate.push(cube);
+    cube.position.set(position.x, position.y, position.z);
+}
 
 function init() {
     scene = new three.Scene();
@@ -14,10 +24,31 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new three.BoxGeometry(0.5, 0.5, 2);
-    var material = new three.MeshBasicMaterial({ color: 0x00ff00 });
-    cube = new three.Mesh(geometry, material);
-    scene.add(cube);
+    const randscale = 5;
+
+    for (let i = 0; i < 100; i++) {
+        const position = new three.Vector3(
+            Math.random() * randscale - randscale / 2,
+            Math.random() * randscale - randscale / 2,
+            Math.random() * (randscale * 100) - 100,
+        );
+        addCuboid(
+            scene,
+            position,
+        );
+    }
+
+    // const light = new three.PointLight(undefined, 1, 15, 5);
+    // light.position.set(0, 0, -0.5);
+    // scene.add(light);
+
+    const light2 = new three.PointLight(0xff11ff, 20, 20, 10);
+    light2.position.set(0, 0, -0.2);
+    scene.add(light2);
+
+    // addCuboid(scene, [0, 0, 0]);
+    // addCuboid(scene, [0, 1, 0]);
+    // addCuboid(scene, [0, -1, 0]);
 
     camera.position.z = 5;
 }
@@ -25,13 +56,17 @@ function init() {
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.06;
-    cube.rotation.y += 0.1;
+    for (const mesh of meshesToAnimate) {
+        mesh.rotation.x += 0.06;
+        mesh.rotation.y += 0.1;
+        const gravity = new three.Vector3(0, 0, 0.1);
+        mesh.position.add(gravity);
+    }
 
     renderer.render(scene, camera);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     init();
     animate();
 });
