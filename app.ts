1d77553e98d3f0b5dab8ebd6202137a70a772fb4
Julian <julianymark@gmail.com>
2018-07-23 21:39:31 +0200

    refactor: extract functions (init and animate)

--- app.ts
+++ app.ts
@@ -1,29 +1,37 @@
 import './main.scss';
 import * as three from 'three';
 
-document.addEventListener('DOMContentLoaded', function () {
-    var scene = new three.Scene();
-    var camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 600);
+var scene: three.Scene;
+var camera: three.Camera;
+var renderer: three.WebGLRenderer;
+var cube: three.Mesh;
 
-    var renderer = new three.WebGLRenderer();
+function init() {
+    scene = new three.Scene();
+    camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 600);
+
+    renderer = new three.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new three.BoxGeometry(1, 1, 1);
+    var geometry = new three.BoxGeometry(0.5, 0.5, 2);
     var material = new three.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new three.Mesh(geometry, material);
+    cube = new three.Mesh(geometry, material);
     scene.add(cube);
 
     camera.position.z = 5;
+}
 
-    function animate() {
-        requestAnimationFrame(animate);
+function animate() {
+    requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
+    cube.rotation.x += 0.06;
+    cube.rotation.y += 0.1;
 
-        renderer.render(scene, camera);
-    }
+    renderer.render(scene, camera);
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+    init();
     animate();
 });
