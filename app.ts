2490e47d83c9e3c4283f055ff0a23d0a3135ddf0
Julian <julianymark@gmail.com>
2018-07-23 21:19:48 +0200

    fast rotating green cube

--- app.ts
+++ app.ts
@@ -2,11 +2,28 @@ import './main.scss';
 import * as three from 'three';
 
 document.addEventListener('DOMContentLoaded', function () {
-    // var scene = new three.Scene();
-    // var camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 600);
+    var scene = new three.Scene();
+    var camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 600);
 
     var renderer = new three.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    var geometry = new three.BoxGeometry(1, 1, 1);
+    var material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new three.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function animate() {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.1;
+        cube.rotation.y += 0.1;
+
+        renderer.render(scene, camera);
+    }
+
+    animate();
 });
